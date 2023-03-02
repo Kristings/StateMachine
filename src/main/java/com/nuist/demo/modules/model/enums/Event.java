48350f13c19abc3dff7d50cb25b3469173e1be61
GuoZhan <1452463601@qq.com>
2023-03-02 17:28:23 +0800

    修改状态和事件的定义

--- src/main/java/com/nuist/demo/modules/model/enums/Event.java
+++ src/main/java/com/nuist/demo/modules/model/enums/Event.java
@@ -1,9 +1,10 @@
 package com.nuist.demo.modules.model.enums;
 
 public enum Event {
-    
-    // RECEIVED_USER_REQUEST,
-    // RECEIVED_NODE_REQUEST,
-    // RECEIVED_NODE_RESPONSE,
-    // TIME_OUT;
+
+    RECEIVED_USER_REQUEST, // 收到应用层的请求
+    RECEIVED_NODE_REQUEST, // 收到其他节点的请求
+    RECEIVED_NODE_RESPONSE, // 收到其他节点的响应
+    DECISION_MADE, // 达成共识
+    TIME_OUT; // 交易超时
 }
